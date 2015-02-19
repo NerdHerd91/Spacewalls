ef77ada1e589c6f27a2fe2bea4b20245f8890fcb
Justin Capogna <jcapogna@gmail.com>
2015-02-19 14:30:49 -0600

    add windows support

--- background_changer/setbackground.py
+++ background_changer/setbackground.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python
 
+import ctypes
 import os
 from os.path import expanduser
 import platform
@@ -8,12 +9,14 @@ import time
 from threading import Thread
 import urllib
 
+# windows call to set wallpaper
+SPI_SETDESKWALLPAPER = 20
 
 image_url = "http://lorempixel.com/1920/1080/";
 dest_path = expanduser("~") + "/image.jpg"
 
 platform = platform.system()
-if not(platform == "Linux"):
+if not(platform == "Linux" or platform == "Windows"):
     print "Your platform (" + platform + ") is not supported."
     sys.exit(-1)
 
@@ -26,7 +29,11 @@ def download(image_url, dest_path):
 
 def changeBackground():
     download(image_url, dest_path)
-    os.system("gsettings set org.gnome.desktop.background picture-uri file://" + dest_path)
+    if platform == "Linux":
+        os.system("gsettings set org.gnome.desktop.background picture-uri file://" + dest_path)
+    if platform == "Windows":
+        print "changing windows background"
+        ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, dest_path, 0)
     print "background changed"
 
 
