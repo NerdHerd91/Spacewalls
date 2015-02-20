0b4719113eb386cd8dac602858b03ee8e1e65efc
vdewinter <vdewinter@i-106023.local>
2015-02-19 20:25:45 -0600

    Adding seed file to populate db

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from sqlalchemy import TIMESTAMP
+from sqlalchemy import create_engine, Sequence, TIMESTAMP
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
 
 from flask.ext.heroku import Heroku
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/spacewalls'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/spacewalls'
 heroku = Heroku(app)
 db = SQLAlchemy(app)
 
+engine = create_engine('postgresql://localhost/spacewalls', echo=True)
+session = scoped_session(sessionmaker(bind = engine,
+                                    autocommit = False,
+                                    autoflush = False))
+Base = declarative_base()
+Base.query = session.query_property()
+
 # database model
-class Wallpapers(db.Model):
+class Wallpapers(Base):
 	__tablename__ = 'wallpapers'
 
 	id = db.Column(db.Integer, primary_key=True)
---
+++ seed.py
@@ -0,0 +1,28 @@
+import app, json, datetime
+
+def load_pics(session):
+	with open('./static/seed_data/items.json') as f:
+		decoded_json = json.load(f) # check
+
+		for i in decoded_json:
+			path = 'static/images/' + i['title'] # does this need an extension?
+			url = i['link'] 
+			last_modified = datetime.datetime.now()
+
+			entry = app.Wallpapers(path=path, url=url, last_modified=last_modified)
+
+			print entry
+
+			session.add(entry)
+		session.commit()
+
+
+def main(session):
+	app.Base.metadata.create_all(bind=app.engine)
+	load_pics(session)
+
+if __name__ == "__main__":
+	main(app.session)
+			
+			
+
